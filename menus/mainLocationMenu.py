338edfba6cc3dd84ad1adaa56bfaa5b66622a653
Cyborger <joe.zlonicky@gmail.com>
2018-05-08 21:14:16 -0700

    Added forest image

--- menus/mainLocationMenu.py
+++ menus/mainLocationMenu.py
@@ -9,7 +9,7 @@ class MainLocationMenu(Menu):
         self.addButton(LabelButton("Travel", 20, 300, self.placeholder))
         self.addButton(LabelButton("Rest", 20, 350, self.rest))
         self.addButton(LabelButton("Player Stats", 20, 400, self.playerStats))
-        self.addLabel(Surface("res/images/campfire.png", 425, 200))
+        self.addLabel(Surface("res/images/forest.png", 425, 200))
 
     def rest(self):
         self.getRoot().fadeMenuChange("locationState/mainLocationMenu", rate=1)
