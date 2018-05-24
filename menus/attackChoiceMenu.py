b8c27d0978e8737015332146d673067f9e972648
Cyborger <joe.zlonicky@gmail.com>
2018-05-24 11:46:40 -0700

    Need to figure out enemy buttons

--- menus/attackChoiceMenu.py
+++ menus/attackChoiceMenu.py
@@ -1,6 +1,12 @@
 from lib.menu import Menu
+from lib.gui.button import Button
 
 
 class AttackChoiceMenu(Menu):
     def __init__(self, parentState):
         super().__init__("attackChoiceMenu", parentState)
+
+
+class EnemyButton(Button):
+    def __init__(self):
+        pass
